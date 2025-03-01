85b535d2b1604d36e260226d8e275452df768bf5
Riccardo Gulmini <riccardogulmini.rg@gmail.com>
2025-03-01 13:55:06 +0100

    Add camera detection check and launch face_detection.py and video_processing_launch.py scripts

--- launch_face_detection.sh
+++ launch_face_detection.sh
@@ -4,7 +4,7 @@
 python3 -c "
 import cv2
 
-cap = cv2.VideoCapture(1)  # 0 is the default camera device
+cap = cv2.VideoCapture(0)  # 0 is the default camera device
 if not cap.isOpened():
     print('Error: Camera not found or cannot be accessed.')
     exit(1)
@@ -14,5 +14,10 @@ cap.release()
 "
 
 # If the camera is detected, launch your face_detection.py script
-echo "Launching face_detection.py..."
-python3 ./face_detection.py
+if [ $? -eq 0 ]; then
+    echo "Launching face_detection.py..."
+    python3 ./face_detection.py
+else
+    echo "Exiting script due to camera access issue."
+    exit 1
+fi
